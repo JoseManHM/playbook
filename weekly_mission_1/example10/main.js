43432eeb1e9f5f698facc3be7232b467d7cb3db7
Manuel Hernández <66561936+JoseManHM@users.noreply.github.com>
2022-04-11 20:27:02 -0500

    Add ejercicios semana 1

---
+++ weekly_mission_1/example10/main.js
@@ -0,0 +1,9 @@
+import MyPokemon from './pokemon.js'
+
+const pikachu = new MyPokemon('Pikachu')
+pikachu.sayHello()
+pikachu.sayMessage("Que onda perro")
+
+const bulbasaur = new MyPokemon('Bulbasaur')
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Que pedro pinche pablo")
