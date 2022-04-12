bf46d318b472f4ab8b04c38afa37fc57ff892c0e
Manuel Hernández <66561936+JoseManHM@users.noreply.github.com>
2022-04-11 20:39:54 -0500

    Delete example10 directory

--- example10/main.js
+++
@@ -1,9 +0,0 @@
-import MyPokemon from './pokemon.js'
-
-const pikachu = new MyPokemon('Pikachu')
-pikachu.sayHello()
-pikachu.sayMessage("Que onda perro")
-
-const bulbasaur = new MyPokemon('Bulbasaur')
-bulbasaur.sayHello()
-bulbasaur.sayMessage("Que pedro pinche pablo")
